a5bca3a521a92e4c02dd46ec31776a9da96a6106
Peerapat <peerapat.p@houseofdev.tech>
2024-08-20 19:33:57 +0700

    20/08

--- Peerapat/src/vite-env.d.ts
+++ Peerapat/src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
+declare module '*.vue' {
+    import { DefineComponent } from 'vue';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
